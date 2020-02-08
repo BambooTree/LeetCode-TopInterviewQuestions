cd2681e8ca3e0670315238ae0c2ac0fa26710e42
BambooTree/koneti <60153392+BambooTree@users.noreply.github.com>
2020-02-08 21:30:16 +0530

    Add files via upload

---
+++ Task Scheduler.cpp
@@ -0,0 +1,48 @@
+#include <iostream> 
+#include <vector> 
+#include <algorithm> 
+#include<bits/stdc++.h> 
+using namespace std;
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char ,int> hash;
+	int maxCount = 0, maxCountTasks = 0;
+        
+	
+    for(char t : tasks)
+    {
+        int  count = hash[t-'A']++;
+        hash.insert(make_pair(t,++count));
+
+        maxCount = max(maxCount, count);
+    }
+    for(int i = 0; i < 26; i++)
+    {
+        if(hash[i] == maxCount) 
+        maxCountTasks++;
+    }
+      
+    int result = (maxCount - 1) * (n + 1) + maxCountTasks;
+	return max((int)tasks.size(),result );
+        
+    }
+};
+
+void printArray(vector<char> arr) 
+{ 
+    for (int i=0; i < arr.size(); i++) 
+        cout << arr[i] << " "; 
+    cout << "\n"; 
+}
+
+int main()
+{
+    vector<char>tasks{'A','A','A','B','B','B'};
+    int k=2;
+    printArray(tasks);
+    int res = Solution().leastInterval(tasks,k);
+    cout<<"res  "<<res<<endl;
+    return 0;
+}
+
