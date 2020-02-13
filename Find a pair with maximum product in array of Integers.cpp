ff23770426e476666bfbb7a07b064c37e21f15f7
BambooTree/koneti <60153392+BambooTree@users.noreply.github.com>
2020-02-13 22:25:04 +0530

    Add files via upload

---
+++ Find a pair with maximum product in array of Integers.cpp
@@ -0,0 +1,85 @@
+// A O(n) C++ program to find maximum product pair in an array 
+#include<bits/stdc++.h> 
+using namespace std; 
+
+void maxProduct_BruteForce(int arr[], int n) 
+{ 
+    if (n < 2) 
+    { 
+        cout << "No pairs exists\n"; 
+        return; 
+    } 
+  
+    // Initialize max product pair 
+    int a = arr[0], b = arr[1]; 
+  
+    // Traverse through every possible pair 
+    // and keep track of max product 
+    for (int i=0; i<n; i++) 
+      for (int j=i+1; j<n; j++) 
+         if (arr[i]*arr[j] > a*b) 
+            a = arr[i], b = arr[j]; 
+  
+    cout << "Max product pair is {" << a << ", "
+         << b << "}"; 
+} 
+
+// Function to find maximum product pair in arr[0..n-1] 
+void maxProduct(int arr[], int n) 
+{ 
+	if (n < 2) 
+	{ 
+		cout << "No pairs exists\n"; 
+		return; 
+	} 
+
+	if (n == 2) 
+	{ 
+		cout << arr[0] << " " << arr[1] << endl; 
+		return; 
+	} 
+
+	// Iniitialize maximum and second maximum 
+	int posa = INT_MIN, posb = INT_MIN; 
+
+	// Iniitialize minimum and second minimum 
+	int nega = INT_MIN, negb = INT_MIN; 
+
+	// Traverse given array 
+	for (int i = 0; i < n; i++) 
+	{ 
+		// Update maximum and second maximum if needed 
+		if (arr[i] > posa) 
+		{ 
+			posb = posa; 
+			posa = arr[i]; 
+		} 
+		else if (arr[i] > posb) 
+			posb = arr[i]; 
+
+		// Update minimum and second minimum if needed 
+		if (arr[i] < 0 && abs(arr[i]) > abs(nega)) 
+		{ 
+			negb = nega; 
+			nega = arr[i]; 
+		} 
+		else if(arr[i] < 0 && abs(arr[i]) > abs(negb)) 
+			negb = arr[i]; 
+	} 
+
+	if (nega*negb > posa*posb) 
+		cout << "Max product pair is {" << nega << ", "
+			<< negb << "}"; 
+	else
+		cout << "Max product pair is {" << posa << ", "
+			<< posb << "}"; 
+} 
+
+// Driver program to test above function 
+int main() 
+{ 
+	int arr[] = {1, 4, 3, 6, 7, 0}; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+	maxProduct(arr, n); 
+	return 0; 
+} 
